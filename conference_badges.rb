312e35927aa68bbcc05439b56ef1faa455d8a826
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 18:04:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
  end
 
 def batch_badge_creator(listOfNames)
-  return "Hello, my name is #{listOfNames).""
+  return "Hello, my name is #{listOfNames)."
 end
 
 # def assign_rooms(listOfNames) 
