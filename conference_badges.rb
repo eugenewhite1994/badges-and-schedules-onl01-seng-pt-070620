ccb7d85accc146873eb0c2fba103e74ab72da7eb
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 17:07:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+def batch_badge_creator(listOfNames)
+  listOfNames.each do |name| 
+    puts "Hello, my name is #{name}"
+  end
+end
+batch_badge_creator(listOfNames)
+
+def assign_rooms 
+  totalRooms = [1, 2, 3, 4, 5, 6, 7]
+  listOfNames.each do |name|
+  totalRooms.each do |room|
+    puts "Hello #{name}! You'll be assigned to room #{room}!"
+  end
+end
+end
+
