02ebde62959a108b02cd2e96a8af06c4e74a47ae
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 18:04:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
- listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+# listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
 def badge_maker(name)
    return "Hello, my name is #{name}."
  end
