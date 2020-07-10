62608421072872a74bce0c62f0edf5e0b837514f
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 19:28:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,4 +30,9 @@ def assign_rooms(array)
 
   return array
 end
+def printer(array)
 
+
+  batch_badge_creator(array).each { |value| puts value }
+  assign_rooms(array).each { |value| puts value }
+end
