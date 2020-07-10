a63e714faf1baed8f50bcab8dc3cc9063a3f0ebb
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 17:19:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 
 
 def assign_rooms(listOfNames) 
-  totalRooms < 7
+  totalRooms = [1, 2, 3, 4, 5, 6, 7]
   listOfNames.each do |name|
   totalRooms.each do |room|
     puts "Hello #{name}! You'll be assigned to room #{room}!"
