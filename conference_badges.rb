fcf7ddd586983810782d2882c11749b480c10c5e
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 17:28:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,13 +6,13 @@ end
 end
 
 
-def assign_rooms(listOfNames) 
-totalRooms = [1, 2, 3, 4, 5, 6, 7]
-listOfNames.each do |name|
-totalRooms.each do |room|
-puts "Hello #{name}! You'll be assigned to room #{room}!"
-end
-end
-end
-assign_rooms(listOfNames)
+# def assign_rooms(listOfNames) 
+# totalRooms = [1, 2, 3, 4, 5, 6, 7]
+# listOfNames.each do |name|
+# totalRooms.each do |room|
+# puts "Hello #{name}! You'll be assigned to room #{room}!"
+# end
+# end
+# end
+# assign_rooms(listOfNames)
 
