bbc07a8f756b0b8d597e3a6ea1edbb23d7303d45
Eugene White <eugenewhite1994@gmail.com>
2020-07-10 17:22:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,24 +1,19 @@
-# listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
-# def badge_maker(listOfNames)
-#   listOfNames.each do |name| 
-#     puts ("Hello, my name is #{name}.")
-#   end
-# end
-
-
-# def assign_rooms(listOfNames) 
-#   totalRooms = [1, 2, 3, 4, 5, 6, 7]
-#   listOfNames.each do |name|
-#   totalRooms.each do |room|
-#     puts "Hello #{name}! You'll be assigned to room #{room}!"
-#   end
-# end
-# end
-# assign_rooms(listOfNames)
-listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
-
-def badge_maker(names_array)
-  names_array.each do |name| 
-    puts ("Hello, my name is #{name}.")
-  end
+ listOfNames = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+def badge_maker(listOfNames)
+  listOfNames.each do |name| 
+  puts ("Hello, my name is #{name}.")
 end
+end
+
+
+def assign_rooms(listOfNames) 
+totalRooms = [1, 2, 3, 4, 5, 6, 7]
+listOfNames.each do |name|
+totalRooms.each do |room|
+puts "Hello #{name}! You'll be assigned to room #{room}!"
+end
+end
+end
+assign_rooms(listOfNames)
+listOfNames
+
